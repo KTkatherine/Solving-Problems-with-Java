49e0e619b3fa0abba024ca09bfe4a4d8170eac34
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-03-07 19:41:39 +1100

    Add files via upload

--- sum_of_factorials_14.java
+++ sum_of_factorials_14.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class sum_of_factorials_14 {
+    /**
+     Take the sum of factorials
+
+        Calculate 1! + 2! + 3! ... ... + n!
+     */
+    public static void main(String[] args) {
+        System.out.println("Please enter an integer:");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        //calculate sum of the factorial values from 1 ... n
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += factor(i);
+        }
+        System.out.println(sum);
+
+
+
+    }
+
+    public static int factor (int num) {
+        int ret = 1;
+        for (int i = 1; i <= num; i++) {
+            ret *= i;
+        }
+        return ret;
+    }
+
+
+
 }
