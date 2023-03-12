905a3d2dfb67dd80e234024532a3e60c1244a305
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-03-12 19:25:22 +1100

    Add files via upload

--- maximum_method_overloaded_17.java
+++ maximum_method_overloaded_17.java
@@ -1,10 +1,42 @@
 public class maximum_method_overloaded_17 {
     /**
-     Calculate the maximum by using hte 
+     Calculate the maximum by using the method overload
 
+     Define multiple methods in the same class:
 
      */
     public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        double c = 20.1;
+        double d = 40.5;
+
+        int max1 = max(a,b);
+        System.out.println(max1);//
+        System.out.println(max(a,b));//和上面的输出方法一样
+        System.out.println(max(c,d));
+        System.out.println(max(c,d,b));
+        //30
+        //30
+        //40.5
+        //40.5
 
     }
+
+    public static int max(int a, int b){
+        return a > b ? a : b;
+    }
+
+    public static double max(double a, double b){
+        return a > b ? a : b;
+    }
+
+    public static double max (double a,double b, int c){//方法参数个数不同
+        double tempMax = max(a,b);
+        double tempC = c + 0.0;//或者c * 1.0 把c 变成double 类型
+        return tempMax > tempC ? tempMax : tempC;
+    }
+
+
+
 }
