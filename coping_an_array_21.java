d1d645ae1d95b9f9d43937188faa67229f649ec5
KTkatherine <123142893+KTkatherine@users.noreply.github.com>
2023-04-08 03:51:15 +1000

    Add files via upload

--- coping_an_array_21.java
+++ coping_an_array_21.java
@@ -10,7 +10,10 @@ public class coping_an_array_21 {
     public static void main(String[] args) {
         int[] data = new int[] {1,3,5,};
         int[] newData = copyOf(data);
+        //在程序中如何证明newData是一块新的数组空间,而不是指向原data数组 --->改data数组里的值,看原data 数组里面的值变不变
+        data[0] = 10;//没有产生任何影响,说明我们这里的确是两块内存区.
         printArray(newData);//1 3 5
+        printArray(data);//10 3 5 
 
     }
 
